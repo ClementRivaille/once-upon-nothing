3f5127da96220920d675242bdfe6012409bc4079
rivaille-c <rivaille-c@mgdis.fr>
2016-09-12 14:24:18 +0200

    feat: Angular 2 hello world
    Why was it so hard, seriously?

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 var nodemon = require('gulp-nodemon');
+var connect = require('gulp-connect');
+var openBrowser = require('gulp-open');
 
 function compile() {
   return tsProject.src()
@@ -10,14 +12,38 @@ function compile() {
 }
 gulp.task('compile', compile);
 
-function run() {
-  return nodemon({
-    script: 'build/main.js'
-  })
-  .on('exit', function() {
-    process.exit();
+// Run the development server
+gulp.task('connect', ['compile'], function() {
+  return connect.server({
+    root: ['.'],
+    port: 4057,
+    livereload: {
+      port: 35740
+    }
   });
-}
-gulp.task('run', run);
+});
 
-gulp.task('default', ['compile'], run);
+gulp.task('open', ['connect'], function() {
+  return gulp.src('index.html').pipe(openBrowser('', {
+    url: 'http://localhost:4057/index.html#/'
+  }));
+});
+
+/*-----------------------------------
+WATCH
+----------------------- */
+
+// // Watch all file modifications then trigger tasks dedicated to file types
+// gulp.task('reload', function() {
+//   return gulp.src(['app/modules/**/*.js', 'app/src/**/*.js']).pipe(connect.reload());
+// });
+// gulp.task('watch', function() {
+//   livereload.listen();
+//   gulp.watch('app/**/*.html', gulp.parallel(['templates', 'reload']));
+//   gulp.watch('app/**/*.js', gulp.parallel(['js', 'reload']));
+//   gulp.watch('app/**/*.less', gulp.parallel(['less', 'reload']));
+//   gulp.watch('app/index.html', gulp.parallel(['inject', 'reload']));
+// });
+
+
+gulp.task('default', ['compile', 'connect', 'open']);
---
+++ src/app/components/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'once-upon-nothing-app',
+  templateUrl: 'src/app/components/app/app.component.html'
+})
+export class AppComponent { 
+  title = 'Once Upon Nothing'
+}
