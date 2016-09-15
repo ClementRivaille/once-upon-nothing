9e1cf5691ea66d3d1a69dff1ef3c0fc8df3336f0
rivaille-c <rivaille-c@mgdis.fr>
2016-09-15 14:58:32 +0200

    chore: sourcemaps & local server

--- gulpfile.js
+++ gulpfile.js
@@ -4,45 +4,47 @@ var tsProject = ts.createProject('tsconfig.json');
 var nodemon = require('gulp-nodemon');
 var connect = require('gulp-connect');
 var openBrowser = require('gulp-open');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
 
+// Build js
 function compile() {
   return tsProject.src()
+    .pipe(sourcemaps.init())
     .pipe(ts(tsProject))
-    .js.pipe(gulp.dest('build'));
+    .js.pipe(sourcemaps.write('.', {
+      sourceRoot: function(file) {
+        return file.cwd + '/src';
+      },
+      sourceMappingURLPrefix: function (file) {
+        var mapPath = file.path.split(/[\\\/]/);
+        return 'http://localhost:4057/build/' + mapPath.slice(1, mapPath.length - 1).join('/');
+      }
+    }))
+    .pipe(gulp.dest('build'));
 }
 gulp.task('compile', compile);
 
 // Run the development server
-gulp.task('connect', ['compile'], function() {
-  return connect.server({
-    root: ['.'],
+gulp.task('serve', ['compile'], function() {
+  return browserSync.init({
+    server: {
+      baseDir: './'
+    },
     port: 4057,
-    livereload: {
-      port: 35740
-    }
+    startPath: '/index.html'
   });
 });
 
-gulp.task('open', ['connect'], function() {
-  return gulp.src('index.html').pipe(openBrowser('', {
-    url: 'http://localhost:4057/index.html#/'
-  }));
+// Watch
+gulp.task('reload-js', ['compile'], function() {
+  browserSync.reload();
+  done();
 });
-
-/*-----------------------------------
-WATCH
------------------------ */
-
-// Watch all file modifications then trigger tasks dedicated to file types
-// gulp.task('reload', function() {
-//   return gulp.src(['app/modules/**/*.js', 'app/src/**/*.js']).pipe(connect.reload());
-// });
 gulp.task('watch', ['compile'], function() {
-  gulp.watch('./src/**/*.ts', ['compile']);
-//   gulp.watch('app/**/*.js', gulp.parallel(['js', 'reload']));
-//   gulp.watch('app/**/*.less', gulp.parallel(['less', 'reload']));
-//   gulp.watch('app/index.html', gulp.parallel(['inject', 'reload']));
+  gulp.watch('./src/**/*.ts', ['compile', 'reload-js']);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
 });
 
-
-gulp.task('default', ['compile', 'connect', 'open', 'watch']);
+// Main
+gulp.task('default', ['compile', 'serve', 'watch']);
